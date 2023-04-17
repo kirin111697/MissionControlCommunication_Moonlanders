df875b0dda0b1dec0c2183b6c06951b3c87e3951
kirin111697 <kirinonfire6373@gmail.com>
2023-04-17 16:21:12 -0400

    set up reload game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -64,12 +64,12 @@ public class GameManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 ResetGame();
-            }
+            }            
+        }
 
-            if (Input.GetKeyDown(KeyCode.Backspace))
-            {
-                ReloadGame();
-            }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ReloadGame();
         }
     }
 
